406f417d11cbe1200372e2c980141e734235b284
aslamsujath29 <aslamsujath@thaagam.email>
2026-03-26 11:05:20 +0530

    my local changes before pull

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('signup/',         views.signup_view,          name='signup'),
     path('login/',          views.login_view,           name='login'),
     path('logout/',         views.logout_view,          name='logout'),
+    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('public-dashboard/', views.public_dashboard,  name='public_dashboard'),
     path('accept-invite/<uuid:token>/', views.accept_invite_view, name='accept_invite'),
     path('profile/',        views.profile_view,         name='profile'),
