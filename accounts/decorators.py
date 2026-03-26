8a114a898367f61be86e6d577a1e1c98a60786cf
johnweslee92 <johnwesleejohnweslee@gmail.com>
2026-03-26 09:56:44 +0530

    Updated accounts module and login flow

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -5,18 +5,12 @@ from django.urls import reverse
 
 
 def role_required(*roles):
-    """
-    Protects a view to only the given roles.
-    Usage:
-        @role_required('teacher')
-        @role_required('teacher', 'management')
-    """
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(request, *args, **kwargs):
             if not request.user.is_authenticated:
                 return redirect(reverse('login'))
-            if request.user.role not in roles:
+            if not request.user.is_superuser and request.user.role not in roles:
                 messages.error(request, 'You do not have permission to access this page.')
                 return redirect(request.user.get_dashboard_url())
             return view_func(request, *args, **kwargs)
