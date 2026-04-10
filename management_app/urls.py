f90b45b91e554971e50e23fca8e2c312de1efb88
JohnWeslee97 <johnwesleejohnweslee@gmail.com>
2026-04-10 18:22:13 +0530

    updates , management

--- management_app/urls.py
+++ management_app/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('dashboard/',      views.dashboard,     name='management_dashboard'),
     path('invite-teacher/', views.invite_teacher, name='management_invite_teacher'),
     path('teachers/',       views.teachers_list,  name='management_teachers_list'),
+    path('announcements/',  views.announcements_management, name='management_announcements'),
+    path('announcements/<str:mode>/', views.announcements_detail, name='management_announcements_detail'),
 ]
